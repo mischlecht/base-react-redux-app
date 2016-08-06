d8a517cb15c00f05c725c51bc8a752ae5b6fd33b
mischlecht <mischlecht@gmail.com>
2016-08-06 10:58:55 -0500

    Update About Page

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -11,7 +11,7 @@ export default class AboutPage extends React.Component {
     render() {
         return <div>
             <h2 className="alt-header">About</h2>
-            <p>This example app is part of the <a href="https://github.com/mischlecht/base-react-redux-app">React-Slingshot starter kit</a>.</p>
+            <p>This app demonstrates the <a href="https://github.com/mischlecht/base-react-redux-app">Base React/Redux App</a> based off of the <a href="https://github.com/coryhouse/react-slingshot">React-Slingshot starter kit</a>.</p>
             <p>
                 <Link to="/badlink">Click this bad link</Link> to see the 404 page.
             </p>
