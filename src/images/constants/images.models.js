9419410c1ac51ac0d0b17cb605b0b5b08b42f8fe
Michael Schlecht <mischlecht@gmail.com>
2017-05-24 09:22:47 -0500

    Replace Beach Images

--- src/images/constants/images.models.js
+++ src/images/constants/images.models.js
@@ -8,11 +8,11 @@ export const Image = Immutable.Record({
 export const InitialState = Immutable.fromJS({
     image1: new Image({
         photoId: 'image1',
-        srcUrl: 'https://s3.us-east-2.amazonaws.com/hackweekend2017-image-storage/BeachPhotos/beach.png'
+        srcUrl: 'http://ste.india.com/sites/default/files/2016/01/21/452974-monkey.jpg'
     }),
     image2: new Image({
         photoId: 'image2',
-        srcUrl: 'https://s3.us-east-2.amazonaws.com/hackweekend2017-image-storage/BeachPhotos/beach2.0.png'
+        srcUrl: 'https://www.longleat.co.uk/api/v2/storage/public/assets/25/monkey-mayhem/monkey-grid-1_original_original.jpg'
     }),
 
 
